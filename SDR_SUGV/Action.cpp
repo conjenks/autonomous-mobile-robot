3cc81697c35e5983480bd62298756bd3550792c4
Ubuntu User <cjconnorjenkins@gmail.com>
2018-04-02 21:42:36 -0400

    auto driving algorithm not working

--- SDR_SUGV/Action.cpp
+++ SDR_SUGV/Action.cpp
@@ -5,7 +5,7 @@
 #include "Localization.h"
 #include "SDR_SUGV.h"
 
-
+void stop();
 void updateMotor() {
 
   if (autoFlag == -1) { // manual driving
@@ -55,23 +55,32 @@ void updateMotor() {
   if (autoFlag == 1) { // auto driving here
 
     if (sonarAverage < SONAR_STOP_DISTANCE) { // stop-- WILL NEED TO BE ADJUSTED - NOISE IS NOT FULLY ACCOUNTED FOR
-    Serial.println("obstacle detected -- STOPPED");
-      digitalWrite(MOTOR_RIGHT_A, LOW); 
-      digitalWrite(MOTOR_RIGHT_B, LOW); 
-      analogWrite(MOTOR_RIGHT_ENABLE, 0);
-      digitalWrite(MOTOR_LEFT_A, LOW); 
-      digitalWrite(MOTOR_LEFT_B, LOW); 
-      analogWrite(MOTOR_LEFT_ENABLE, 0);
+      Serial.println("obstacle detected -- STOPPED");
+      stop();
     }
-    else if (lineL == 1) { // we're over the right line, turn left for .3 seconds then move straight
-    Serial.println("right line auto called");
-      digitalWrite(MOTOR_RIGHT_A, HIGH); 
-      digitalWrite(MOTOR_RIGHT_B, LOW); 
-      analogWrite(MOTOR_RIGHT_ENABLE, 255);
-      digitalWrite(MOTOR_LEFT_A, HIGH); 
-      digitalWrite(MOTOR_LEFT_B, LOW); 
-      analogWrite(MOTOR_LEFT_ENABLE, 100);
-      delay(300);
+    else if (lineR == 1) { // we're over the left line
+      while (lineR == 1) { // turn right while we're over the left line
+        stop();
+        delay(500);
+        digitalWrite(MOTOR_RIGHT_A, HIGH); 
+        digitalWrite(MOTOR_RIGHT_B, LOW); 
+        analogWrite(MOTOR_RIGHT_ENABLE, -255);
+        digitalWrite(MOTOR_LEFT_A, HIGH); 
+        digitalWrite(MOTOR_LEFT_B, LOW); 
+        analogWrite(MOTOR_LEFT_ENABLE, 0);
+        stop();
+        delay(500);
+        digitalWrite(MOTOR_RIGHT_A, HIGH); 
+        digitalWrite(MOTOR_RIGHT_B, LOW); 
+        analogWrite(MOTOR_RIGHT_ENABLE, -255);
+        digitalWrite(MOTOR_LEFT_A, HIGH); 
+        digitalWrite(MOTOR_LEFT_B, LOW); 
+        analogWrite(MOTOR_LEFT_ENABLE, 255);
+        delay(500);
+        stop();
+        lineR = digitalRead(LINE_RIGHT);
+      } // NOW CONTINUE MOVING
+      Serial.println("right line auto called");
       digitalWrite(MOTOR_RIGHT_A, HIGH); 
       digitalWrite(MOTOR_RIGHT_B, LOW); 
       analogWrite(MOTOR_RIGHT_ENABLE, 255);
@@ -79,15 +88,28 @@ void updateMotor() {
       digitalWrite(MOTOR_LEFT_B, LOW); 
       analogWrite(MOTOR_LEFT_ENABLE, 255);
     }
-    else if (lineR == 1) { // we're over the left line, turn right for .3 seconds then move straight
+    else if (lineL == 1) { // we're over the right line
+      while (lineL == 1) { // turn left while we're over the right line
+        stop();
+        delay(500);
+        digitalWrite(MOTOR_RIGHT_A, HIGH); 
+        digitalWrite(MOTOR_RIGHT_B, LOW); 
+        analogWrite(MOTOR_RIGHT_ENABLE, 0);
+        digitalWrite(MOTOR_LEFT_A, HIGH); 
+        digitalWrite(MOTOR_LEFT_B, LOW); 
+        analogWrite(MOTOR_LEFT_ENABLE, -255);
+        stop();
+        delay(500);
+        digitalWrite(MOTOR_RIGHT_A, HIGH); 
+        digitalWrite(MOTOR_RIGHT_B, LOW); 
+        analogWrite(MOTOR_RIGHT_ENABLE, 255);
+        digitalWrite(MOTOR_LEFT_A, HIGH); 
+        digitalWrite(MOTOR_LEFT_B, LOW); 
+        analogWrite(MOTOR_LEFT_ENABLE, -255);
+        delay(500);
+        lineL = digitalRead(LINE_LEFT);
+      }
       Serial.println("left line auto called");
-      digitalWrite(MOTOR_RIGHT_A, HIGH); 
-      digitalWrite(MOTOR_RIGHT_B, LOW); 
-      analogWrite(MOTOR_RIGHT_ENABLE, 100);
-      digitalWrite(MOTOR_LEFT_A, HIGH); 
-      digitalWrite(MOTOR_LEFT_B, LOW); 
-      analogWrite(MOTOR_LEFT_ENABLE, 255);
-      delay(300);
       digitalWrite(MOTOR_RIGHT_A, HIGH); 
       digitalWrite(MOTOR_RIGHT_B, LOW); 
       analogWrite(MOTOR_RIGHT_ENABLE, 255);
@@ -107,6 +129,19 @@ void updateMotor() {
 
   }
 }
+
+void stop() {
+  digitalWrite(MOTOR_RIGHT_A, LOW); 
+  digitalWrite(MOTOR_RIGHT_B, LOW); 
+  analogWrite(MOTOR_RIGHT_ENABLE, 0);
+  digitalWrite(MOTOR_LEFT_A, LOW); 
+  digitalWrite(MOTOR_LEFT_B, LOW); 
+  analogWrite(MOTOR_LEFT_ENABLE, 0);
+}
+
+
+
+
 
 
 
